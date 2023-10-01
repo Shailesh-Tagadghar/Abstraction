0de64a04f5f061c01221645eb92eab4f543af577
Shailesh-Tagadghar <shaileshtagadghar13@gmail.com>
2023-10-01 12:13:51 +0530

    new comments

--- src/Day1/DemoInterface.java
+++ src/Day1/DemoInterface.java
@@ -5,6 +5,7 @@ public interface DemoInterface {
     int num = 10;
     double PI = 3.14;
     //final : declaration
+    //final var value assignment : at the time of declaration only.
     //in interface we can not create constructor because they have static value so we can't create instance variable as well.
 
     //public and abstract methods
